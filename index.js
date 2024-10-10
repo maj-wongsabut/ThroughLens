21657363868aa14e7fbe7f4878667280aed145a9
Supattra Maj Wongsabut <maj.wongsabut@outlook.com>
2024-10-10 11:45:33 +0200

    files and codes added

---
+++ index.js
@@ -0,0 +1,10 @@
+// Hamburgare-menu 
+function showSidebar(){
+  let sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+
+function closedSidebar(){
+  let sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
